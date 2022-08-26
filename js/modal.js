9ca10cb129c5dc3d05133d45abc9b78bf1f0f5b3
YPK <flusnukyura@gmail.com>
2022-08-26 17:03:52 +0000

    v1

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+(function() {
+    const modal = document.querySelector(".modal");
+    const trigger = document.querySelector(".trigger");
+    const closeButton = document.querySelector(".close-button");
+    
+    function toggleModal(ev) {
+        modal.classList.toggle("show-modal");
+        ev.stopPropagation();
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    modal.addEventListener("click", toggleModal);
+})();
